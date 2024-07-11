be396b5550b80399aef419465002435d630ba4a4
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-07-12 01:28:20 +0300

    make build

---
+++ index.6ed72beb.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),l=e.offsetWidth,i=e.offsetHeight,o=t.clientWidth,c=t.clientHeight;e.style.left=(o-l)/2+"px",e.style.top=(c-i)/2+"px";
+//# sourceMappingURL=index.6ed72beb.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const widthSpider = spider.offsetWidth;
+const heightSpider = spider.offsetHeight;
+const widthWall = wall.clientWidth;
+const heightWall = wall.clientHeight;
+spider.style.left = (widthWall - widthSpider) / 2 + "px";
+spider.style.top = (heightWall - heightSpider) / 2 + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
